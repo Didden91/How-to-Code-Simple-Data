7ef3f53ffee12738cc693dde512e4192e1f7b473
Didden91 <81488820+Didden91@users.noreply.github.com>
2021-06-27 23:10:18 +0200

    Create strings.rkt

---
+++ strings.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname strings) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+"banana"
+"John"
+
+(string-append "John" "Johnson")
+
+(string-append "John" " " "Johnson")
+
+(string-length "Johnny")
+
+(substring "Johnnyboy" 2 6)
+
+(substring "012345678" 2 6)
