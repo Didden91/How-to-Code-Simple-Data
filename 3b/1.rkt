7364d8be89962f2f718e95f83db998e40a9cd7ca
Didden91 <81488820+Didden91@users.noreply.github.com>
2021-07-19 21:15:03 +0200

    Create 1.rkt

---
+++ 3b/1.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+
+(define-struct pos (x y))
+
+(define P1 (make-pos 3 6)) ; constructors
+(define P2 (make-pos 2 8))
+
+(pos-x P1) ; selectors
+(pos-y P2)
+
+(pos? P1) ; predicate
+(pos? "hello") ;
